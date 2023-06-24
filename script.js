6da3cbf90f231a75fc2fcb3e14c23800ad002af0
Amaan2775 <119450302+Amaan2775@users.noreply.github.com>
2023-06-24 05:03:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
     {songName: "Desperate", filePath: "songs/1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "Cielo Huma-Huma", filePath: "songs/2.mp3", coverPath: "cover/2.jpg"},
+    {songName: "Cielo", filePath: "songs/2.mp3", coverPath: "cover/2.jpg"},
     {songName: "DownBeat", filePath: "songs/3.mp3", coverPath: "cover/3.jpg"},
     {songName: "Dynamiser", filePath: "songs/4.mp3", coverPath: "cover/4.jpg"},
 ]
